837d6bacdeb93782267bdbfd93ce9f71d5c89924
kot3u <25024243+kot3u@users.noreply.github.com>
2023-03-14 11:48:12 +0100

    Functioning version w/o additional fearures

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const myLibrary = [];
 const submitBtn = document.querySelector('#submit-button');
-const newBookBtn = document.querySelector('#new-book-btn')
+const newBookBtn = document.querySelector('#new-book-btn');
 
-function Book(author, tilte, pages, read) {
+function Book(author, tilte, pages) {
   this.author = author;
   this.tilte = tilte;
   this.pages = pages;
-  this.read = read;
 }
 
 function deleteBook(evt, bookObject) {
   const parent = evt.target.parentNode;
-  parent.innerHTML = '';
+  parent.remove();
   myLibrary.pop(bookObject);
 }
 
@@ -33,41 +32,60 @@ function addParagraphs(bookObject, div) {
 }
 
 function showForm() {
-  const formContainer = document.querySelector('#input-form-container')
-  formContainer.classList.add('active')
+  const newBookBtnContainer = document.querySelector('#new-book-btn-container');
+  const formContainer = document.querySelector('#input-form-container');
+  formContainer.classList.remove('not-active');
+  newBookBtnContainer.classList.add('not-active');
 }
 
-function hideForm(evt) {
-  evt.target.parentNode.classList.remove('active')
+function hideForm() {
+  const newBookBtnContainer = document.querySelector('#new-book-btn-container');
+  const formContainer = document.querySelector('#input-form-container');
+  formContainer.classList.add('not-active');
+  newBookBtnContainer.classList.remove('not-active');
+}
+
+function setReadStatus(evt) {
+  if (evt.textContent !== "You've read this book!") {
+    evt.parentNode.classList.add('is-read');
+    return "You've read this book!";
+  }
+  evt.parentNode.classList.remove('is-read');
+  return "You heve't read this book!";
+}
+
+function addReadButton(div) {
+  const readBtn = document.createElement('button');
+  readBtn.classList.add('read-button');
+  readBtn.textContent = 'Reading status';
+  readBtn.addEventListener('click', (evt) => {
+    evt.target.textContent = setReadStatus(evt.target);
+  });
+  div.appendChild(readBtn);
 }
 
 function display(bookObject) {
-  const booksContainer = document.querySelector('.books-container');
+  const booksContainer = document.querySelector('main');
   const div = document.createElement('div');
   div.classList.add('container');
   addParagraphs(bookObject, div);
+  addReadButton(div);
   addButton(bookObject, div);
-  booksContainer.appendChild(div);
+  booksContainer.insertBefore(div, booksContainer.firstChild);
 }
 
-function addBookToLibrary(author, tilte, pages, read) {
-  const book = new Book(author, tilte, pages, read);
+function addBookToLibrary(author, tilte, pages) {
+  const book = new Book(author, tilte, pages);
   myLibrary.push(book);
   display(book);
 }
 
-submitBtn.addEventListener('click', (evt) => {
+submitBtn.addEventListener('click', () => {
   const authorInput = document.querySelector('#author-input');
   const tilteInput = document.querySelector('#title-input');
   const pagesInput = document.querySelector('#pages-input');
-  const readInput = document.querySelector('#read-input');
-  addBookToLibrary(
-    authorInput.value,
-    tilteInput.value,
-    pagesInput.value,
-    readInput.value,
-  );
-  hideForm(evt)
+  addBookToLibrary(authorInput.value, tilteInput.value, pagesInput.value);
+  hideForm();
 });
 
-newBookBtn.addEventListener('click', showForm)
+newBookBtn.addEventListener('click', showForm);
