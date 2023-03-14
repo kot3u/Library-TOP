f9d4360120827d7cb413906c0a49bdb8f3148659
kot3u <25024243+kot3u@users.noreply.github.com>
2023-03-14 10:15:56 +0100

    Version with popup div

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const myLibrary = [];
-const authorInput = document.querySelector('#author-input');
-const tilteInput = document.querySelector('#title-input');
-const pagesInput = document.querySelector('#pages-input');
-const readInput = document.querySelector('#read-input');
 const submitBtn = document.querySelector('#submit-button');
-const body = document.querySelector('body');
+const newBookBtn = document.querySelector('#new-book-btn')
 
 function Book(author, tilte, pages, read) {
   this.author = author;
@@ -13,16 +9,45 @@ function Book(author, tilte, pages, read) {
   this.read = read;
 }
 
-function display(bookObject) {
-  const div = document.createElement('div');
-  div.classList.add('container');
+function deleteBook(evt, bookObject) {
+  const parent = evt.target.parentNode;
+  parent.innerHTML = '';
+  myLibrary.pop(bookObject);
+}
+
+function addButton(bookObject, div) {
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('remove-button');
+  removeButton.textContent = 'Remove Book';
+  removeButton.addEventListener('click', deleteBook);
+  div.appendChild(removeButton);
+}
+
+function addParagraphs(bookObject, div) {
   Object.values(bookObject).forEach((value) => {
     const paragraph = document.createElement('p');
     paragraph.classList.add('book-data');
     paragraph.textContent = value;
     div.appendChild(paragraph);
   });
-  body.appendChild(div);
+}
+
+function showForm() {
+  const formContainer = document.querySelector('#input-form-container')
+  formContainer.classList.add('active')
+}
+
+function hideForm(evt) {
+  evt.target.parentNode.classList.remove('active')
+}
+
+function display(bookObject) {
+  const booksContainer = document.querySelector('.books-container');
+  const div = document.createElement('div');
+  div.classList.add('container');
+  addParagraphs(bookObject, div);
+  addButton(bookObject, div);
+  booksContainer.appendChild(div);
 }
 
 function addBookToLibrary(author, tilte, pages, read) {
@@ -31,12 +56,18 @@ function addBookToLibrary(author, tilte, pages, read) {
   display(book);
 }
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', (evt) => {
+  const authorInput = document.querySelector('#author-input');
+  const tilteInput = document.querySelector('#title-input');
+  const pagesInput = document.querySelector('#pages-input');
+  const readInput = document.querySelector('#read-input');
   addBookToLibrary(
     authorInput.value,
     tilteInput.value,
     pagesInput.value,
     readInput.value,
   );
-  document.querySelectorAll('input').value = '';
+  hideForm(evt)
 });
+
+newBookBtn.addEventListener('click', showForm)
